c6ad7b5d83494469c83e5e2ef683a95f3dada6fd
sergey_j9 <sergei.zhulevich@guild-of-developers.ru>
2025-10-08 23:25:12 +0300

    Создан pipeline для выдачи JWT-токена

---
+++ service/pipelines.py
@@ -0,0 +1,24 @@
+import json
+import logging
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.http import HttpResponse
+
+logger = logging.getLogger(__name__)  # Логгер для вывода сообщений
+
+def jwt_response_with_refresh_token(strategy, details, backend, request=None, *args, **kwargs):
+    user = kwargs['user']  # Текущий пользователь
+    refresh = RefreshToken.for_user(user)  # Генерация JWT-токена
+    response_data = {
+        'access': str(refresh.access_token),
+        'refresh': str(refresh),
+        'user_id': user.id,
+        'email': user.email,
+        'role': user.role,  # Возвращается роль пользователя
+    }
+
+    # Выводим токены и данные пользователя в консоль
+    # logger.info(f"Access Token (токен доступа): {response_data['access']}")
+    # logger.info(f"Refresh Token (обновляющий токен): {response_data['refresh']}")
+    # logger.info(f"Данные пользователя:\nID пользователя: {response_data['user_id']}\nЭлектронная почта: {response_data['email']}\nРоль: {response_data['role']} ({response_data['role'].capitalize()})")
+
+    return HttpResponse(json.dumps(response_data), content_type='application/json')
